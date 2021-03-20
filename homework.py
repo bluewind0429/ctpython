b8459699016cd825cdca1af2da397bb5b360ee7d
bluewind0429 <80206424+bluewind0429@users.noreply.github.com>
2021-03-20 21:27:58 +0800

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 12:15:54 2021
+
+@author: user
+"""
+for i in range(5,0,-1):
+
+    for a in range (0,i):
+        print("*",end='')
+    print()
+
+
+for i in range(2,6,+1):
+
+    for a in range (0,i):
+        print("*",end='')
+    print()
+
+print('Finish')
